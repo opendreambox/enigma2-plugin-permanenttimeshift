f9d5bcb14a9395229a917dea6aab5f77c60f6d75
Sven H <svenho@arcor.de>
2018-08-24 21:10:33 +0200

    PTS 2.0 (OE2.5) - own display-screen and real remaining time - new: show eventname in the new TimeShiftState-Screen "PTSStandardTimeshiftState" - new: on "Stop-Key" show the TimeShiftState-Screen with STOP-State for 1 second - new: show TimeShiftState-Screen after ptsTimeshiftFileChanged - new: own Display-Screen to show the eventname of the timeshiftevent in the display - new: show real remainingtime on timeshifting of the current liveEvent (setup-option) - new: PTS Infobar show real timeline (3 colors: grey, blue, yellow) - new: show Question to zap on active timeshift - new: on active timeshift use key "<" and ">" to jump to begin/end of active timeshift - fix: irgnore keys "2", "5", "8" while Timeshifting - fix: Stop-Key to switchToLive - this key works most only on second time

---
+++ Components/Converter/PTSLiveEventPosition.py
@@ -0,0 +1,58 @@
+﻿#!/usr/bin/python
+# encoding: utf-8
+#
+# original from EMCServicePosition
+# Copyright (C) 2011 betonme
+#
+# In case of reuse of this source code please do not remove this copyright.
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	For more information on the GNU General Public License see:
+#	<http://www.gnu.org/licenses/>.
+#
+
+from Components.Converter.ServicePosition import ServicePosition
+from Components.Element import cached
+
+class PTSLiveEventPosition(ServicePosition):
+	def __init__(self, type):
+		ServicePosition.__init__(self, type)
+		self.doSuspend(1) #Stop the poll timer that ServicePosition.__init__(...) starts!
+
+	@cached
+	def getCutlist(self):
+		# Not used
+		return []
+
+	cutlist = property(getCutlist)
+
+	@cached
+	def getPosition(self):
+		player = self.source.player
+		position = player and player.getLivePosition()
+		if position:
+			return position
+		# Fallback
+		return ServicePosition.getPosition(self)
+
+	position = property(getPosition)
+
+	@cached
+	def getLength(self):
+		player = self.source.player
+		length = player and player.getLength()
+		if length:
+			return length
+		# Fallback
+		return ServicePosition.getLength(self)
+
+	length = property(getLength)
